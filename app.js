859f0160bdc5fd95c577dad76317f7ce9c0fad3b
ashwin-97 <ashelangovan97@gmail.com>
2017-07-21 19:43:08 +0530

    Second commit

---
+++ app.js
@@ -0,0 +1,72 @@
+var express    = require("express");
+var mysql      = require('mysql');
+var app = express();
+ require('dotenv').config();
+var connectionInfo = {
+  host     : 'localhost',  // mysql server hostname 
+  user     : 'root',       // mysql database user name
+  password : 'root',       // mysql database password
+  database : 'mroom'  // mysql database name
+}
+ 
+var connection = mysql.createConnection(connectionInfo);
+ 
+// Test Case #1 : To test mysql connectivity
+exports.testMySqlConnectivity = function(test){
+     connection.connect(function(err){
+         if(err) {
+            test.ok(false, "Error to connect database.");
+          } else {
+            test.ok(true, "Database connected successfully.");
+          }
+          test.done();
+     }); 
+};
+ 
+ 
+// Test Case #2 : To check expected value against actual value.
+exports.testRowsCount = function(test){
+     var query = 'SELECT * FROM rooms';
+        connection.query(query, function(err, rows, fields) {
+            var expectedRows = 70;
+            var actualRows = rows.length;
+            //connection.end();
+             if(err) {
+                test.ok(false, "Error to execute query.");
+
+             } else {
+                test.equal(
+                    actualRows, 
+                    expectedRows, 
+                    "Actual result is: "+actualRows +", Expected result is: "+expectedRows
+                );
+ 
+             }
+             test.done();
+ 
+     }); 
+ 
+};
+
+exports.updaterooms = function(test){
+     var query = 'SELECT * FROM rooms where roomname="napierbridge"';
+        connection.query(query, function(err, rows, fields) {
+            var expectedRows = 1;
+            var actualRows = rows[0].calendar;
+            connection.end();
+             if(err) {
+                test.ok(false, "Error to execute query.");
+             } else {
+                test.equal(
+                    actualRows, 
+                    expectedRows, 
+                    "Actual result is: "+actualRows +", Expected result is: "+expectedRows
+                );
+ 
+             }
+             test.done();
+ 
+     }); 
+ 
+}; 
+ 
